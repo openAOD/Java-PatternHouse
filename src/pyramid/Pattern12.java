01d1fc698dee537499c720652798f21416da5c91
ChlorinePentoxide <chlorinepentoxide@gmail.com>
2021-03-21 17:18:13 +0530

    Add all pyramid patterns courtesy of previous developers

---
+++ src/pyramid/Pattern12.java
@@ -0,0 +1,18 @@
+class Pattern12 {
+    public static void main(String args[])
+    {
+        int n=4; //size
+        for (int i=1; i<=n; i++)
+        {
+            for (int j=n-1; j>=i; j--)
+            {
+                System.out.print(" ");
+            }
+            for (int k=i-1; k>=-(i-1); k--)
+            {
+                System.out.print(i-Math.abs(k));
+            }
+            System.out.println();
+        }
+    } //end of main
+}  //end of class
