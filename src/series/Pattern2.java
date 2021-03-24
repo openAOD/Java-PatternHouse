93a03caecd7ef43a773d63c56d78d100e7b878b9
ChlorinePentoxide <chlorinepentoxide@gmail.com>
2021-03-24 20:43:53 +0530

    Adding series patterns ...

---
+++ src/series/Pattern2.java
@@ -0,0 +1,18 @@
+/** 
+ * Contributed
+ * 		to: PatternHouse
+ *		by: ChlorinePentoxide
+ * Copyright(C) 2021, PatternHouse
+ * The MIT License
+ **/
+
+public class Pattern2 {
+
+	public static void main(String[] args) {
+		double power = 2.0d;
+		int numberOfTerms = 100;
+		
+		for(int i=0;i<numberOfTerms;i++) System.out.print( Math.pow(i, power) + " ");
+	}
+
+}
