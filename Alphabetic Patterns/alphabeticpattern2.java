ec722279a94df6475769cc82aac83d481cc824fd
Kajal Kumari <80222700+Kajal13081@users.noreply.github.com>
2021-12-02 23:25:37 +0530

    Add alphabetic pattern 1 and 2

--- Alphabetic Patterns/alphabeticpattern2.java
+++ Alphabetic Patterns/alphabeticpattern2.java
@@ -1,11 +1,11 @@
 // program to print following pattern
-// A A A A A
-// B B B B B
-// C C C C C
-// D D D D D
-// E E E E E
+// A B C D E
+// A B C D E
+// A B C D E
+// A B C D E
+// A B C D E
 
-public class alphabeticpattern1 {
+public class alphabeticpattern2 {
 
     public static void main(String[] args) {
 
@@ -17,13 +17,9 @@ public class alphabeticpattern1 {
         int alphabet = 65;
         for (char i = 0 ; i < n ; i++) {
             for (char j = 0 ; j < n ; j++) {
-                System.out.print((char)(alphabet) + " ");
+                System.out.print((char)(alphabet + j) + " ");
             }
-            alphabet++;
             System.out.println();
         }
     }
 }
-
-
-
