e9a1a75e8452a70a859b25275367502a7a483e27
Kajal Kumari <80222700+Kajal13081@users.noreply.github.com>
2021-12-03 16:19:32 +0530

    Add Alphabetic pattern 9 and 10

---
+++ Alphabetic Patterns/alphabeticpattern10.java
@@ -0,0 +1,25 @@
+// program to print following pattern
+// A
+// A B
+// A B C
+// A B C D
+// A B C D E
+
+public class alphabeticpattern10 {
+
+    public static void main(String[] args) {
+
+        int rows = 5;
+        alphabet_rectangle(rows);
+    }
+    static void alphabet_rectangle(int n){
+
+        int alphabet = 64;
+        for (char i = 1 ; i <= n ; i++) {
+            for (char j = 1 ; j <= i ; j++) {
+                System.out.print((char)(alphabet + j) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
