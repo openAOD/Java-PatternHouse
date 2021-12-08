628adc3f85c695ab69364402e1554cd6ae2aa245
Kajal Kumari <80222700+Kajal13081@users.noreply.github.com>
2021-12-08 15:53:53 +0530

    Added pattern 12,13

---
+++ Alphabetic Patterns/alphabeticpattern12.java
@@ -0,0 +1,25 @@
+// program to print following pattern
+// E
+// D D
+// C C C
+// B B B B
+// A A A A A
+
+public class alphabeticpattern12 {
+
+    public static void main(String[] args) {
+
+        int rows = 5;
+        alphabet_triangle(rows);
+    }
+    static void alphabet_triangle(int n){
+
+        int alphabet = 64;
+        for (char i = (char) (n); i > 0 ; i--) {
+            for (char j = (char) (n); j >=i ; j--) {
+                System.out.print((char)(alphabet + i) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
