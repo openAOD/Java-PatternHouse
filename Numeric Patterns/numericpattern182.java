0a8f635349b9c043085e8eefccb47fbe085329a7
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2021-12-12 23:30:39 +0530

    added from numericpattern179 to numericpattern 189

---
+++ Numeric Patterns/numericpattern182.java
@@ -0,0 +1,17 @@
+
+
+public class numericpattern182 {
+    public static void main(String[] args) {
+        int n=5;
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= row; col++) {
+                if (col % 2 == 0){
+                    System.out.print("* ");
+                }else{
+                    System.out.print(row + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Numeric Patterns/numericpattern186.java
@@ -0,0 +1,17 @@
+
+
+public class numericpattern186 {
+    public static void main(String[] args) {
+        int n=5;
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <=n- row+ 1; col++) {
+                if (col % 2 == 0){
+                    System.out.print("* ");
+                }else{
+                    System.out.print(n-row+1 + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
