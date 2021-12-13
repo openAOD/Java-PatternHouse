c0213ef446e55b42af1284495ec952de1fadc2ce
Bobby Kumar <kashyapashish0002@gmail.com>
2021-12-13 15:53:30 +0530

    Added code for pyramid pattern 11

---
+++ Pyramid Patterns/pyramidpattern11.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class pyramidpattern11 {
+
+    static void printPattern (int totalLines) {
+
+        int whiteSpaces, printStar;
+        char Character;
+
+        for (int i = totalLines; i >= 1; i--) {
+
+            whiteSpaces = (totalLines - i);
+            printStar = ((2 * i) - 1);
+
+            Character = 'A';
+
+            for (int j = 1; j <= whiteSpaces; j++) {
+
+                System.out.print(" ");
+            }
+
+            for (int k = 1; k <= printStar; k++) {
+
+                if((k & 1) != 0) {
+
+                    System.out.print(Character++);
+                } else {
+
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println();
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner (System.in);
+
+        int totalLines;
+
+        System.out.print("How many lines to be printed? --> ");
+        totalLines = input.nextInt();
+
+        System.out.println("\nYour pattern looks like this:\n");
+        printPattern (totalLines);
+    }
+}
