a38a50b382fd6c33715909f7807b69a270337a95
Abhirv08 <abhishekk.ic.20@nitj.ac.in>
2021-12-17 11:47:25 +0530

    added numericpattern223

---
+++ Numeric Patterns/numericpattern223.java
@@ -0,0 +1,24 @@
+
+//pattern to be printed out
+//5 * * * * *
+//4 * * * *
+//3 * * *
+//2 * *
+//1 *
+//0
+
+public class numericpattern223 {
+    public static void main(String[] args) {
+        int n = 6;
+        for (int row = 1; row <= n ; row++) {
+            for (int col = 1; col <= n-row+1; col++) {
+                if (col == 1){
+                    System.out.print(n-row + " ");
+                }else{
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
