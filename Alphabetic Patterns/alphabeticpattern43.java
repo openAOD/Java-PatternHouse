2c29aa3f2566d28de5e192fcd7a449ddf9e598b7
TAMAL BAG <87414354+tamalbag117@users.noreply.github.com>
2021-12-19 20:34:14 +0530

    Add files via upload

---
+++ Alphabetic Patterns/alphabeticpattern43.java
@@ -0,0 +1,34 @@
+//pattern to be printed
+//EEEEEEEEE
+// DDDDDDD
+//  CCCCC
+//   BBB
+//    A
+
+/**
+ * alphabeticpattern43
+ */
+public class alphabeticpattern43 {
+
+    
+    public static void main(String[] args) {
+
+        int rows = 5;
+        alphabetPattern43(rows);
+    }
+
+    private static void alphabetPattern43(int n) {
+
+        int alphabet = 64;
+        for (char i = (char) n; i > 0; i--) {
+            for (int index = n; index >=i; index--) {
+                System.out.print("  ");
+            }
+            for (int j = 2*i-1; j > 0; j--) {    
+                System.out.print((char) (alphabet + i) + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
