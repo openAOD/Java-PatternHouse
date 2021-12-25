763150f3cad89e52eb2bcb08669befba0df683ca
Abhirv08 <abhishekk.ic.20@nitj.ac.in>
2021-12-25 21:29:20 +0530

    added numericpattern250

---
+++ Numeric Patterns/numericpattern250.java
@@ -0,0 +1,26 @@
+
+//pattern to be printed
+//        1
+//      2 2
+//    3   3
+//  4     4
+//5 5 5 5 5
+
+public class numericpattern250 {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int row = 1; row <= n; row++) {
+            for (int spaces = 1; spaces <= n-row; spaces++) {
+                System.out.print("  ");
+            }
+            for (int col = 1; col <= row; col++) {
+                if (col == row || col == 1 || row == n){
+                    System.out.print(row + " ");
+                }else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
