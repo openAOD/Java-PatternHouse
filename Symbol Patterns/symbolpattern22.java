285b58cbf0921863e0832bddf99424ea1d30b112
TAMAL BAG <87414354+tamalbag117@users.noreply.github.com>
2021-12-26 20:43:07 +0530

    Commit Patterns

---
+++ Symbol Patterns/symbolpattern22.java
@@ -0,0 +1,33 @@
+/**
+ * pattern to be printed 
+ * 
+ * E E E E E 
+ * D D D D *
+ * C C C * *
+ * B B * * *
+ * A * * * *
+ * 
+ * symbolpattern22
+ */
+public class symbolpattern22 {
+    public static void main(String[] args) {
+        int rows = 5;
+        symbolPattern22(rows);
+    }
+    private static void symbolPattern22(int n) {
+        int  alphabet = 64;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <=n-i; j++) {
+                System.out.print((char)(alphabet+n-i+1)+" ");
+            }
+        
+            for (int j = 1; j < i; j++) {
+                System.out.print("* ");
+                
+            }
+            System.out.println();
+        }
+
+    }
+
+}
---
+++ Symbol Patterns/symbolpattern25.java
@@ -0,0 +1,36 @@
+/**
+ * pattern to be printed 
+ * 
+      * * * * A
+      * * * B A
+      * * C B A
+      * D C B A
+      E D C B A     
+
+ * 
+ * symbolpattern25
+ */
+public class symbolpattern25 {
+    public static void main(String[] args) {
+        int rows = 5;
+        symbolPattern25(rows);
+    }
+    
+    private static void symbolPattern25(int n) {
+        int  alphabet = 64;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <n-i; j++) {
+                System.out.print("* ");
+            }
+        
+            for (int j = 1; j <=i; j++) {
+                System.out.print((char)(alphabet+i-j+1)+" ");
+                
+            }
+            System.out.println();
+        }
+
+    }
+
+}
+
