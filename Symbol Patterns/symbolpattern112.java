d767243f371c227b6a83206e93d24eefca6f7939
Yashesvinee V <76458668+Yashesvinee@users.noreply.github.com>
2021-12-27 22:07:58 +0530

    Update symbolpattern112.java

--- Symbol Patterns/symbolpattern112.java
+++ Symbol Patterns/symbolpattern112.java
@@ -1,11 +1,22 @@
-n = 5
-for i in range (n):
-    j = -1
-    for j in range (n-i-1):
-        print(' ', end='')
-    for k in range (2*i+1) :
-        if (j+k)%2 != 0:
-            print('*', end='')
-        else:
-            print('#', end='')
-    print()
+class Main 
+{     
+    public static void main(String[] args) 
+        {
+        int n=5;
+        for(int i=0;i<n;i++)
+        {
+          int j=-1;
+          for(j=0; j<n-i-1; j++)
+          System.out.print(" ");
+
+          for(int k=0; k<2*i+1; k++)
+          {
+            if ((j+k) % 2!=0)
+            System.out.print("*");
+            else 
+            System.out.print('#');
+          }
+          System.out.println();
+          }
+        }  
+    }   
