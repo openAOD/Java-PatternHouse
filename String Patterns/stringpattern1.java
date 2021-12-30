e62cd74e83bdd62c3046e9c2db0a330065576775
Mahak008 <88129955+Mahak008@users.noreply.github.com>
2021-12-30 15:03:51 +0530

    Create stringpattern1.java

---
+++ String Patterns/stringpattern1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class StringPattern1
+{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String str = s.nextLine();
+        int n = s.nextInt();
+        int l = str.length();
+        
+        for(int i=0;i<n;i++) {
+            System.out.println(str);
+        }
+    }
+}
