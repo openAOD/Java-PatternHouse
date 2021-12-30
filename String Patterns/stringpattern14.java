f602dba04c6e5c145dcbee0964ea088b306a1725
Mahak008 <88129955+Mahak008@users.noreply.github.com>
2021-12-30 19:35:26 +0530

    Create stringpattern14.java

---
+++ String Patterns/stringpattern14.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class StringPattern3
+{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String str = s.nextLine();
+        int l = str.length();
+        for(int i=0;i<l;i++) {    //loop to print row
+            for(int j=0;j<i+1;j++) {    //loop to print values of upper triangle
+                System.out.print(str.charAt(j));
+            }
+            System.out.println();
+        }
+        for(int i=0;i<l-1;i++) {    //loop to print row
+            for(int j=0;j<l-i-1;j++) {    //loop to print values of lower triangle
+                System.out.print(str.charAt(j));
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
