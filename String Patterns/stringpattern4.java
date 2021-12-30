f041022229920dcd8d23bfa7036b1d05ac88eb63
Mahak008 <88129955+Mahak008@users.noreply.github.com>
2021-12-30 14:25:41 +0530

    Create stringpattern4.java

---
+++ String Patterns/stringpattern4.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class StringPattern3
+{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String str = s.nextLine();
+        int l = str.length();
+        for(int i=0;i<l;i++) {
+            for(int j=0;j<=(l-i-1);j++){
+                System.out.print(str.charAt(j));
+            }
+            System.out.println();
+        }
+    }
+}
