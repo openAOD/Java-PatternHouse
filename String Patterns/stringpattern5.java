67ab72fe467516e17afbbadf0a8e9af2f3e23334
Mahak008 <88129955+Mahak008@users.noreply.github.com>
2021-12-30 14:46:51 +0530

    Create stringpattern5.java

---
+++ String Patterns/stringpattern5.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class StringPattern3
+{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String str = s.nextLine();
+        int l = str.length();
+        for(int i=0;i<l;i++) {
+            for(int j=l-1;j>i;j--){
+                System.out.print(" ");
+            }
+            for(int k=0;k<=i;k++) {
+                System.out.print(str.charAt(k));
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println();
+    }
+}
