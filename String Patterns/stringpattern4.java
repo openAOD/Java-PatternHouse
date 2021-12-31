63faa2b442d7acfa64bf6bb840cce43a4319e081
Mahak008 <88129955+Mahak008@users.noreply.github.com>
2021-12-31 13:42:39 +0530

    Update stringpattern4.java

--- String Patterns/stringpattern4.java
+++ String Patterns/stringpattern4.java
@@ -7,7 +7,7 @@ public class StringPattern4
         int l = str.length();
         for(int i=0;i<l;i++) {    //loop to print row
             for(int j=0;j<=(l-i-1);j++){    //loop to print value
-                System.out.print(str.charAt(j));
+                System.out.print(str.charAt(j) + " ");
             }
             System.out.println();
         }
@@ -16,8 +16,8 @@ public class StringPattern4
 
 // Sample Input :- HOUSE
 // Output :-
-// HOUSE
-// HOUS
-// HOU
-// HO
+// H O U S E
+// H O U S
+// H O U
+// H O
 // H
