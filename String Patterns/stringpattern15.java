433aa916231b70e86e48bb024ec3426bc9436362
Mahak008 <88129955+Mahak008@users.noreply.github.com>
2021-12-31 15:00:37 +0530

    Create stringpattern15.java

---
+++ String Patterns/stringpattern15.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class StringPattern3
+{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String str = s.nextLine();
+        int l = str.length();
+        for(int i=0;i<l;i++) { 
+            for(int j=0;j<l;j++) {
+                if(i==l/2) {
+                    System.out.print(str.charAt(j));
+                }
+                else if(j==l/2) {
+                    System.out.print(str.charAt(i));
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+// Sample Input :- ABCDE
+// Output :-
+//   A
+//   B
+// ABCDE
+//   D
+//   E
