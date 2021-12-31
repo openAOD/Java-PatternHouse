a10aa3a0f0a5bae75bc70812b28d7a6e243f4f05
Mahak008 <88129955+Mahak008@users.noreply.github.com>
2021-12-31 16:32:39 +0530

    Update stringpattern2.java

--- String Patterns/stringpattern2.java
+++ String Patterns/stringpattern2.java
@@ -4,21 +4,21 @@ public class StringPattern2
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
       
-        String str = s.nextLine();
+        String str = s.nextLine(); // Input the String.
         String rev = "";
         
         int l = str.length();
-        int n = s.nextInt();
+        int n = s.nextInt(); // Input no. of times the value to be printed.
         
         char ch;
         
-        for (int i=0; i<l; i++) {
+        for (int i=0; i<l; i++) { // Loop to reverse the string.
             ch= str.charAt(i);
             rev= ch+rev;
         }
         
-        for(int i=0;i<n;i++) {
-            for(int j=0;j<l;j++) {
+        for(int i=0;i<n;i++) { // Loop to print rows.
+            for(int j=0;j<l;j++) { // Loop to print the string
                 System.out.print(rev.charAt(j) + " ");
             }
             System.out.println();
