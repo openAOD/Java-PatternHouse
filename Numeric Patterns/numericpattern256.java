1ad5222cb30f7549c23e46b834ad2c229274820f
Abhirv08 <abhishekk.ic.20@nitj.ac.in>
2021-12-31 20:31:54 +0530

    added numericpattern256

---
+++ Numeric Patterns/numericpattern256.java
@@ -0,0 +1,25 @@
+// pattern to be printed
+//    1
+//   2 2
+//  3   3
+// 4     4
+//5       5
+
+public class numericpattern256 {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int row = 1; row <= n; row++) {
+            for (int spaces = 1; spaces <= n-row; spaces++) {
+                System.out.print(" ");
+            }
+            for (int col = 1; col <= 2*row-1; col++) {
+                if (col == 1 || col == 2*row-1){
+                    System.out.print(row);
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
