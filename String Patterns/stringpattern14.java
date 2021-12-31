70d7b6fd4b1ff89a9088d99d9fa7ba1dd8f0a54c
Mahak008 <88129955+Mahak008@users.noreply.github.com>
2021-12-31 13:20:41 +0530

    Update stringpattern14.java

--- String Patterns/stringpattern14.java
+++ String Patterns/stringpattern14.java
@@ -7,16 +7,26 @@ public class StringPattern14
         int l = str.length();
         for(int i=0;i<l;i++) {    //loop to print row
             for(int j=0;j<i+1;j++) {    //loop to print values of upper triangle
-                System.out.print(str.charAt(j));
+                System.out.print(str.charAt(j) + " ");
             }
             System.out.println();
         }
         for(int i=0;i<l-1;i++) {    //loop to print row
             for(int j=0;j<l-i-1;j++) {    //loop to print values of lower triangle
-                System.out.print(str.charAt(j));
+                System.out.print(str.charAt(j) + " ");
             }
             System.out.println();
         }
         System.out.println();
     }
 }
+
+// Sample Input :- ARYA
+// OUTPUT :-
+// A
+// A R
+// A R Y
+// A R Y A
+// A R Y
+// A R
+// A
