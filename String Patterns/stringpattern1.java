7076e731b8f712a80a5712e6eb2d3b6639124de2
Mahak008 <88129955+Mahak008@users.noreply.github.com>
2021-12-31 16:10:55 +0530

    Update stringpattern1.java

--- String Patterns/stringpattern1.java
+++ String Patterns/stringpattern1.java
@@ -3,12 +3,12 @@ public class StringPattern1
 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        String str = s.nextLine();
-        int l = str.length();
-        int n = s.nextInt();
+        String str = s.nextLine(); // Input String.
+        int l = str.length(); 
+        int n = s.nextInt(); // Input no. of times the value to be printed.
         
-        for(int i=0;i<n;i++) {
-            for(int j=0;j<l;j++) {
+        for(int i=0;i<n;i++) { // Loop to print rows.
+            for(int j=0;j<l;j++) { // Loop to print string.
                 System.out.print(str.charAt(j) + " ");
             }
             System.out.println();
