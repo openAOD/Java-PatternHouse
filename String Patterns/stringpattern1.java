c05b915081cb1d7a135ae41c96f94814245e121f
Mahak008 <88129955+Mahak008@users.noreply.github.com>
2021-12-31 13:27:33 +0530

    Update stringpattern1.java

--- String Patterns/stringpattern1.java
+++ String Patterns/stringpattern1.java
@@ -4,19 +4,23 @@ public class StringPattern1
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         String str = s.nextLine();
+        int l = str.length();
         int n = s.nextInt();
         
         for(int i=0;i<n;i++) {
-            System.out.println(str);
+            for(int j=0;j<l;j++) {
+                System.out.print(str.charAt(j) + " ");
+            }
+            System.out.println();
         }
-    }
+    }   
 }
 
 
 // Sample Input :- HOUSE 
 //                 4
 // Output :-
-// HOUSE
-// HOUSE
-// HOUSE
-// HOUSE
+// H O U S E
+// H O U S E
+// H O U S E
+// H O U S E
