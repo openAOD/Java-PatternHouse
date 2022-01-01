88d3c7d77574201185e1de645f0fc23620683e7e
Mahak008 <88129955+Mahak008@users.noreply.github.com>
2022-01-01 13:56:31 +0530

    Create alphabeticpattern31.java

---
+++ Alphabetic Patterns/alphabeticpattern31.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class AlphabeticPattern31
+{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt(); // Input no. of lines to be printed 
+        
+        for(int i=0;i<n;i++) {
+            for(int j=1;j<n-i;j++) {
+                System.out.print(" ");
+            }
+          
+            for(int k=0;k<=i;k++) {
+                char x = (char)(i+65);
+                System.out.print(x + " ");
+            }
+          
+            System.out.println();
+        }
+    }
+}
+
+// Sample Input :- 5
+// Output :-
+//     A
+//    B B
+//   C C C
+//  D D D D
+// E E E E E
