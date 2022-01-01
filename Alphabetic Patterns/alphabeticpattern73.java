a787137c308ccdb51848b9ea8aa0faf1e0774d6b
Mahak008 <88129955+Mahak008@users.noreply.github.com>
2022-01-01 14:10:30 +0530

    Create alphabeticpattern73.java

---
+++ Alphabetic Patterns/alphabeticpattern73.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class AlphabeticPattern73
+{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt(); // Input no. of lines to be printed
+      
+        for(int i=1;i<=n;i++) {
+            for(int j=1;j<=i;j++) {
+                if(i%2==0) {
+                    System.out.print(j + " ");
+                }
+                else {
+                    System.out.print((char)(j+64) + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+// Sample Input :- 5
+// Output :-
+// A
+// 1 2
+// A B C
+// 1 2 3 4
+// A B C D E
