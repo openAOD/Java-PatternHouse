130f5ac4e98ec0070901a14f1c2ea0097823c051
Mahak008 <88129955+Mahak008@users.noreply.github.com>
2022-01-01 14:32:41 +0530

    Create alphabeticpattern78.java

---
+++ Alphabetic Patterns/alphabeticpattern78.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class AlphabeticPattern78
+{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt(); // Input no. of lines to be printed
+      
+        for(int i=n;i>=1;i--) {
+            for(int j=1;j<=i;j++) {
+              
+                if(i%2==0) {
+                    System.out.print((char)(i+64) + " ");
+                }
+                else {
+                    System.out.print(i + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
