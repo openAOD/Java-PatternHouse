af9a17b375b3d43731e57a1afdd094ce8e722341
Mahak008 <88129955+Mahak008@users.noreply.github.com>
2022-01-01 14:18:04 +0530

    Create alphabeticpattern75.java

---
+++ Alphabetic Patterns/alphabeticpattern75.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class AlphabeticPattern5
+{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt(); // Input no. of lines to be printed
+        
+        for(int i=1;i<=n;i++) {
+            for(int j=1;j<=i;j++) {
+            
+                if(i%2==0) {
+                    System.out.print((char)(j+64) + " ");
+                }
+                else {
+                    System.out.print(j + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+// Sample Input :- 5
+// Output :-
+// 1
+// A B
+// 1 2 3
+// A B C D
+// 1 2 3 4 5
