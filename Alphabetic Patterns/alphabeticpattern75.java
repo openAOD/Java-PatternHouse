ecf1b3fc25969fc85c8d60ee291784c20ee4d592
Mahak008 <88129955+Mahak008@users.noreply.github.com>
2022-01-01 14:19:11 +0530

    Update alphabeticpattern75.java

--- Alphabetic Patterns/alphabeticpattern75.java
+++ Alphabetic Patterns/alphabeticpattern75.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class AlphabeticPattern5
+public class AlphabeticPattern75
 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
