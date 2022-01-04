f1c8d0ddb82dd18609a8f537f8f5b19874a63d7c
Mahak008 <88129955+Mahak008@users.noreply.github.com>
2022-01-04 13:18:12 +0530

    Create alphabeticpattern138.java

---
+++ Alphabetic Patterns/alphabeticpattern138.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class AlphabeticPattern138
+{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        
+        int n = s.nextInt(); // Input no. of lines to be printed
+        for(int i = 1; i <= n; i++) {
+          
+            for(int j = 1; j <= n-i; j++) {
+                System.out.print("   ");
+            }
+          
+            for(int j = 1; j <= i; j++) {
+                System.out.print((char)(j+64) + "  ");
+            }
+          
+            for(int j = 1; j <= i; j++) {
+                System.out.print(j + "  ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// Sample Input :- 5
+// Output :-
+//         A 1
+//       A B 1 2
+//     A B C 1 2 3
+//   A B C D 1 2 3 4
+// A B C D E 1 2 3 4 5
