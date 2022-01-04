c467e23653e0a15d42dbb48b40fa451c7ae991b1
Mahak008 <88129955+Mahak008@users.noreply.github.com>
2022-01-04 12:31:32 +0530

    Create alphabeticpattern119.java

---
+++ Alphabetic Patterns/alphabeticpattern119.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class AlphabeticPattern118
+{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt(); // Input no. of lines to be printed
+        int y = s.nextInt(); // Input no. of times pattern to be printed
+        
+        for(int k = 0; k < y;k++) {
+            for(int i = 1; i <= n;i++) {
+                for(int j = 1; j <= i;j++) {
+                    char c = (char)(j + 64);
+                    System.out.print(c + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
+
+// Sample Input :- 3
+//                 2
+// A
+// A B
+// A B C
+// A
+// A B
+// A B C
+
