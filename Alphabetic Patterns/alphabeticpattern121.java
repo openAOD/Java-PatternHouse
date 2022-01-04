c1d1784a604da8ce29b9e79a9a0bef589996038b
Mahak008 <88129955+Mahak008@users.noreply.github.com>
2022-01-04 12:50:43 +0530

    Update alphabeticpattern121.java

--- Alphabetic Patterns/alphabeticpattern121.java
+++ Alphabetic Patterns/alphabeticpattern121.java
@@ -4,12 +4,12 @@ public class AlphabeticPattern121
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         
-        int n = s.nextInt();
+        int n = s.nextInt(); // Input no. of times pattern is to be printed. 
         
-        for(int k = 0; k <= n; k++) {
-            for(int i = 0; i <= k; i++) {
-                for(int j = 0; j <= i; j++) {
-                    char c = (char)(i+65);
+        for(int k = 1; k <= n; k++) {
+            for(int i = 1; i <= k; i++) {
+                for(int j = 1; j <= i; j++) {
+                    char c = (char)(i+64);
                     System.out.print(c + " ");
                 }
                 System.out.println();
@@ -19,7 +19,7 @@ public class AlphabeticPattern121
     }
 }
 
-// Sample Input :- 2
+// Sample Input :- 3
 // Output :-
 // A
 //
