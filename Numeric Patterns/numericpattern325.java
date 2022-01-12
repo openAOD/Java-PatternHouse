2cc512a8eb281c42aebfc5bbd16f3c1a2b55303e
Kartik Kumar <68912933+cratos166@users.noreply.github.com>
2022-01-12 18:54:43 +0530

    Update numericpattern325.java

--- Numeric Patterns/numericpattern325.java
+++ Numeric Patterns/numericpattern325.java
@@ -1,26 +1,35 @@
 public class numericpattern325 {
     
+    
+      /*    6 6 6 6 6
+            6
+            6 6 6 6 6
+            6       6
+            6 6 6 6 6   */
+    
+    
     public static void main(String[] args) {
 
         int num = 5;
         boolean isRigh = false;
+        int value=6;
 
         for (int i = 0; i < num; i++) {
             if (i % 2 == 0) {
                 for (int j = 0; j < num; j++) {
                     if (j < 4) {
-                        System.out.print(num + " ");
+                        System.out.print(value + " ");
                     } else {
-                        System.out.println(num);
+                        System.out.println(value);
                     }
                 }
             } else {
-                System.out.print(num + " ");
+                System.out.print(value + " ");
                 for (int j = 1; j < num - 1; j++) {
                     System.out.print("  ");
                 }
                 if (isRigh) {
-                    System.out.println(num);
+                    System.out.println(value);
                 } else {
                     isRigh = true;
                     System.out.println("  ");
