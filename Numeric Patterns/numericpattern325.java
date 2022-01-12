f1565730c896745f75c873f0a2a6618d4e89e65d
Kartik Kumar <68912933+cratos166@users.noreply.github.com>
2022-01-12 18:48:48 +0530

    Create numericpattern325.java

---
+++ Numeric Patterns/numericpattern325.java
@@ -0,0 +1,31 @@
+public class Numeric_Patter_325 {
+    
+    public static void main(String[] args) {
+
+        int num = 5;
+        boolean isRigh = false;
+
+        for (int i = 0; i < num; i++) {
+            if (i % 2 == 0) {
+                for (int j = 0; j < num; j++) {
+                    if (j < 4) {
+                        System.out.print(num + " ");
+                    } else {
+                        System.out.println(num);
+                    }
+                }
+            } else {
+                System.out.print(num + " ");
+                for (int j = 1; j < num - 1; j++) {
+                    System.out.print("  ");
+                }
+                if (isRigh) {
+                    System.out.println(num);
+                } else {
+                    isRigh = true;
+                    System.out.println("  ");
+                }
+            }
+        }
+    }
+}
