56c2460685bd7efe762182a6b817d597349657e9
Mahak008 <88129955+Mahak008@users.noreply.github.com>
2022-01-13 18:16:21 +0530

    Create numericpattern42.java

---
+++ Numeric Patterns/numericpattern42.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class NumericPattern42 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int height = s.nextInt(); // Input height of the pattern
+        int x = height, y, num;
+        for (int i = height; i >= 1; i--) {
+        
+            num = x;
+            y = i + 1;
+            
+            for(int j = height; j >= i; j--) {
+            
+                if(num <= 9) {
+                    System.out.print(" " + num + " ");
+                }
+                else {
+                    System.out.print(num + " ");
+                }
+                
+                num = num - y;
+                y++;
+            }
+            x = x + i - 1;
+            System.out.println();
+        }
+    }
+}
+
+// Sample Input :- 5
+// Output :-
+//  5
+//  9  4
+// 12  8  3
+// 14 11  7  2
+// 15 13 10  6  1
