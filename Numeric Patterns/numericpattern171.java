884c7063bb4f509d29344201ecc9286d020f0bab
Mahak008 <88129955+Mahak008@users.noreply.github.com>
2022-01-13 18:34:07 +0530

    Create numericpattern171.java

---
+++ Numeric Patterns/numericpattern171.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class NumericPattern171 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int height = s.nextInt(); // Input height of the pattern
+        int c = 1;
+        
+        for(int i = 1; i <= height; i++) {
+            
+            for(int j = 1; j <= i; j++) {
+                System.out.print(c + "# ");
+                c++;
+            }
+            System.out.println();
+        }
+    }
+}
+
+// Sample Input :- 4
+// Output :-
+// 1#
+// 2# 3#
+// 4# 5# 6#
+// 7# 8# 9# 10#
