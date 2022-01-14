48b94e63f74e3a93a0c561c8abde1853978526bb
Mahak008 <88129955+Mahak008@users.noreply.github.com>
2022-01-14 12:56:41 +0530

    Create numericpattern314.java

---
+++ Numeric Patterns/numericpattern314.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class NumericPattern314 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int height = s.nextInt(); // Input height of the pattern
+        
+        for (int i = 0; i < height; i++) {
+        
+            for (int j = 0; j < height; j++) {
+                
+                if(i == 0 || i == height-1 || i == height/2) {
+                    System.out.print("* ");
+                }
+                else if(i < height/2 && j == 0) {
+                    System.out.print("* ");
+                }
+                else if(i > height/2 && j == height-1) {
+                    System.out.print("* ");
+                }
+                else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+// Sample Input :- 5
+// Output :-
+// * * * * *
+// *
+// * * * * *
+//         *
+// * * * * *
