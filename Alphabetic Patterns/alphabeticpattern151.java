0c6917ffaa3b9af251a6f244300290f1d46b1c97
Kartik Kumar <68912933+cratos166@users.noreply.github.com>
2022-01-15 19:33:52 +0530

    Create alphabeticpattern151.java

---
+++ Alphabetic Patterns/alphabeticpattern151.java
@@ -0,0 +1,28 @@
+/*
+
+ *
+ *
+ *
+ *
+ * * *
+
+ */
+
+
+public class alphabeticpattern151 {
+
+    public static void main(String[] args) {
+        int row = 5;
+        int column = 3;
+        for (int i = 1; i <= row; i++) {
+            if (i == row) {
+                for (int j = 1; j < column; j++) {
+                    System.out.print("* ");
+                }
+                System.out.println("*");
+            } else {
+                System.out.println("*");
+            }
+        }
+    }
+}
