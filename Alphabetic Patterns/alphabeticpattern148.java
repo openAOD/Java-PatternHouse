625171df382a337541b8de4a166c04e2c59ed1b6
Kartik Kumar <68912933+cratos166@users.noreply.github.com>
2022-01-15 19:35:11 +0530

    Create alphabeticpattern148.java

---
+++ Alphabetic Patterns/alphabeticpattern148.java
@@ -0,0 +1,33 @@
+/*
+
+ * * * * *
+     *
+     *
+     *
+ * * * * *
+
+
+ */
+
+public class alphabeticpattern148 {
+
+    public static void main(String[] args) {
+        int row = 5;
+        int column = 5;
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                if (i == 0 || i == row - 1) {
+                    System.out.print("* ");
+                } else {
+                    if (j == column / 2 && j % 2 == 0) {
+                        System.out.print("* ");
+                    } else {
+                        System.out.print("  ");
+                    }
+                }
+            }
+            System.out.println("");
+        }
+    }
+}
