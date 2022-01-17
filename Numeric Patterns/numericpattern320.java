d4a87476dd3f02ce5200c02a08067f98ff0a605d
Mahak008 <88129955+Mahak008@users.noreply.github.com>
2022-01-17 14:05:43 +0530

    Create numericpattern320.java

---
+++ Numeric Patterns/numericpattern320.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class NumericPattern320 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int height = s.nextInt(); // Input height of the pattern
+        
+        for(int i = 1; i <= height; i++) {
+        
+            for(int j = 1; j <= height; j++) {
+            
+                if(i == height || j == height/2+1) {
+                    System.out.print("1 ");
+                }
+                
+                else if(j == height/2-i+1) {
+                    System.out.print("1 ");
+                }
+                
+                else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+// Sample Input :- 5
+// Output :-
+//   1 1
+// 1   1
+//     1
+//     1
+// 1 1 1 1 1
