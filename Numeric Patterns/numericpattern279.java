32a60a3eae02d2ff8aae2d0236ea5ba2d0ad13b3
Mahak008 <88129955+Mahak008@users.noreply.github.com>
2022-01-17 14:03:08 +0530

    Create numericpattern279.java

---
+++ Numeric Patterns/numericpattern279.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class NumericPattern279 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int height = s.nextInt(); // Input height of the pattern
+        int x = 1, y, sum = height*(height+1);
+        
+        for (int i = 1; i <= height; i++) {
+            y = sum-height+i;
+        
+            for (int j = 1; j <= height*2; j++) {
+                
+                if(j > 2*(i-1)) {
+                    if( j <= (height*2)/2+i-1) {
+                        if(x <= 9) {
+                            System.out.print(" " + x++ + " ");
+                        }
+                        else {
+                            System.out.print(x++ + " ");
+                        }
+                    }
+                    else {
+                        if(y <= 9) {
+                            System.out.print(" " + y++ + " ");
+                        }
+                        else {
+                            System.out.print(y++ + " ");
+                        }
+                    }  
+                }
+                else {
+                    System.out.print(" _ ");
+                }
+            }
+            sum = sum - height + i - 1;
+            System.out.println();
+        }
+    }
+}
+
+// Sample Input :- 4
+// Output :-
+// 1  2  3  4 17 18 19 20
+// _  _  5  6  7 14 15 16
+// _  _  _  _  8  9 12 13
+// _  _  _  _  _  _  10 11
